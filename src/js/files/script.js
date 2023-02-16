fdf708ef414375d7f0409adc6ed2b093ffa1901f
Denys Mykhailenko <localworkarea@gmail.com>
2023-02-16 13:15:35 +0200

    update

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,19 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+
+// const secondSection = document.querySelector('[data-fp-id="1"]');
+// // const secondSectionClass = document.querySelector("")
+// const getItemCoords = secondSection.getBoundingClientRect();
+// const getItemTop = secondSection.getBoundingClientRect().top;
+
+// console.log(getItemTop);
+// console.log(getItemCoords);
+
+// if (getItemTop >= 0) {
+//   console.log("больше 0");
+// } 
+// if (getItemTop <=0 ) {
+//   console.log("меньше 0");
+// }
